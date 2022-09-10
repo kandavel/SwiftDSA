47aae36e34ada58736f0e69e98ca15a7ae2cdc93
kandavel-umapathy <kandavel.umapathy@byjys.com>
2022-09-10 23:17:40 +0530

    Created Fibonacci pattern
    Fibonicci Series pattern in swift

--- SwiftDSA.playground/Pages/FibonocciSeries.xcplaygroundpage/Contents.swift
+++ SwiftDSA.playground/Pages/FibonocciSeries.xcplaygroundpage/Contents.swift
@@ -5,11 +5,19 @@ import Foundation
 var greeting = "Hello, playground"
 
 //: [Next](@next)
-func enterTheFibonocciSeriesNumber(number : Int) -> Int {
-    var previousNum: Int = 0
-    var currentNum: Int = 0
+func enterTheFibonocciSeriesNumber(number : Int) -> [Int] {
+    var num1: Int = 0
+    var num2: Int = 1
+    var currentCount  = 0
+    var fibnocciArray : [Int] = []
     
-    
-    
-    
+    for _ in 0..<number {
+        let result  = num1 + num2
+        fibnocciArray.append(num1)
+        num1 = num2
+        num2  = result
+        currentCount = currentCount + 1
+    }
+    return fibnocciArray
 }
+enterTheFibonocciSeriesNumber(number: 10)
